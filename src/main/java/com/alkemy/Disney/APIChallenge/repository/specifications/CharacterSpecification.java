6b60cdc6417593e514d41a273c9274abd320a0bd
Ale S <alesuero93@gmail.com>
2022-06-30 19:34:35 -0300

    Added the posibility of searching by movies id, in the character search by filters

--- src/main/java/com/alkemy/Disney/APIChallenge/repository/specifications/CharacterSpecification.java
+++ src/main/java/com/alkemy/Disney/APIChallenge/repository/specifications/CharacterSpecification.java
@@ -2,10 +2,15 @@ package com.alkemy.Disney.APIChallenge.repository.specifications;
 
 import com.alkemy.Disney.APIChallenge.dto.CharacterFiltersDTO;
 import com.alkemy.Disney.APIChallenge.models.CharacterEntity;
+import com.alkemy.Disney.APIChallenge.models.MovieEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +44,12 @@ public class CharacterSpecification {
                 predicates.add(
                         criteriaBuilder.equal(root.get("weight"), filtersDto.getWeight())
                 );
+            }
+
+            if (!CollectionUtils.isEmpty(filtersDto.getMovies())) {
+                Join<MovieEntity, CharacterEntity> join = root.join("movies", JoinType.INNER);
+                Expression<String> movies_id = join.get("movie_id");
+                predicates.add(movies_id.in(filtersDto.getMovies()));
             }
 
             query.distinct(true);
